cd2810761d105e3c0a12f6cdfe70d90620f06218
handoyonoterakhir <handoyonoterakhir@gmail.com>
2020-05-08 17:24:17 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const labels = document.querySelectorAll(".try-item__label");
+const tabs = document.querySelectorAll(".try-tab");
+
+function toggleShow() {
+	const target = this;
+	const item = target.classList.contains("try-tab")
+		? target
+		: target.parentElement;
+	const group = item.dataset.actabGroup;
+	const id = item.dataset.actabId;
+
+	tabs.forEach(function(tab) {
+		if (tab.dataset.actabGroup === group) {
+			if (tab.dataset.actabId === id) {
+				tab.classList.add("tab-active");
+			} else {
+				tab.classList.remove("tab-active");
+			}
+		}
+	});
+
+	labels.forEach(function(label) {
+		const tabItem = label.parentElement;
+
+		if (tabItem.dataset.actabGroup === group) {
+			if (tabItem.dataset.actabId === id) {
+				tabItem.classList.add("tab-active");
+			} else {
+				tabItem.classList.remove("tab-active");
+			}
+		}
+	});
+}
+
+labels.forEach(function(label) {
+	label.addEventListener("click", toggleShow);
+});
+
+tabs.forEach(function(tab) {
+	tab.addEventListener("click", toggleShow);
+});
